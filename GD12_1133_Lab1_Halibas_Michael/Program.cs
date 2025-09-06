e0fcc8204c783dcd383293f8b18aa75c59a9d5ac
halibmike-svg <halibmike@gmail.com>
2025-09-05 20:53:44 -0700

    exploring visual studio
    commit my programming file

---
+++ GD12_1133_Lab1_Halibas_Michael/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GD12_1133_Lab1_Halibas_Michael
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+  
+}
