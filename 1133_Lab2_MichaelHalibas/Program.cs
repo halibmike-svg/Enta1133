3b57b421a541499a9ec9e460446afa1c6e203781
halibmike-svg <halibmike@gmail.com>
2025-09-08 16:18:22 -0700

    lab 2 publish
    completing lab2 assignment

---
+++ 1133_Lab2_MichaelHalibas/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _1133_Lab2_MichaelHalibas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("decimal 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
+            Console.WriteLine("binary 0, 1, 10, 100, 1000, 10000, 100000, 1000000, 1100100, 11111111");
+            Console.WriteLine("hex 0, 1, 2, 4, 8, 10, 20, 40, 64, FF");
+            Console.WriteLine("message hello, goodbye, Michael Halibas, september 8 2025");
+        }
+      
+        
+
+    }
+}
