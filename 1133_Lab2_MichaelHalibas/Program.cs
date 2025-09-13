8d94e35d0ecda61af4a31b03d47f00f183dd47b3
halibmike-svg <halibmike@gmail.com>
2025-09-12 18:05:01 -0700

    new output
    reorganize and change the output

--- 1133_Lab2_MichaelHalibas/Program.cs
+++ 1133_Lab2_MichaelHalibas/Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("decimal 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
-            Console.WriteLine("binary 0, 1, 10, 100, 1000, 10000, 100000, 1000000, 1100100, 11111111");
-            Console.WriteLine("hex 0, 1, 2, 4, 8, 10, 20, 40, 64, FF");
-            Console.WriteLine("message hello, goodbye, Michael Halibas, september 8 2025");
+            Console.WriteLine("Decimal=Binary=Hexadecimal");
+            Console.WriteLine("   0   =      0    =   0");
+            Console.WriteLine("   1   =      1    =   1");
+            Console.WriteLine("   2   =     10    =   2");
+            Console.WriteLine("   4   =     100   =   4");
+            Console.WriteLine("   8   =    1000   =   8");
+            Console.WriteLine("  16   =   10000   =   10");
+            Console.WriteLine("  32   =  100000   =   20");
+            Console.WriteLine("  64   = 1000000   =   40");
+            Console.WriteLine("  100  = 1100100   =   64");
+            Console.WriteLine("  255  =11111111   =   FF");
+            Console.WriteLine("Hello. My name is Michael Halibas");
+            Console.WriteLine("Date 2025-09-12");
+            Console.WriteLine("Goodbye");
         }
       
         
